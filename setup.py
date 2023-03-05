5956e4be2f71c65832836e773a3f3db77939332b
Niels Ota <nielsota@Nielss-MacBook-Pro-2.local>
2023-03-05 13:28:17 -0500

    newest

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import dist
+dist.Distribution().fetch_build_eggs(['setuptools_rust'])
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+
+setup(
+    name="hello-world-rs",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        ".hello_world_rs.hello_world_rs",
+        path="Cargo.toml", binding=Binding.PyO3)],
+    packages=["hello_world_rsgit "],
+    classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Programming Language :: Rust",
+            "Operating System :: POSIX",
+            "Operating System :: MacOS :: MacOS X",
+        ],
+    zip_safe=False,
+)
